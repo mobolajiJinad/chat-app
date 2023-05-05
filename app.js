c772f01d0920eecd347f6535f18be006309482d4
omobolaji <abdjinadquadri@gmail.com>
2023-05-05 11:56:51 +0100

    Debugging vercel static rendering

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public/"));
 
 app.use(
   session({
