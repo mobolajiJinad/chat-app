996330f68df22bb1bb153c4936ca3f01b8e9204a
Abd'Quadri Jinad <abdjinadquadri@gmail.com>
2023-11-10 23:55:51 +0100

    First commit - change app to use react and tailwindcss

--- routes/routes.js
+++
@@ -1,6 +0,0 @@
-const authRoutes = require("./authRoutes");
-const chatRoutes = require("./chatRoutes");
-const defRoutes = require("./defRoutes");
-const chatListRoutes = require("./chatList");
-
-module.exports = { authRoutes, chatRoutes, defRoutes, chatListRoutes };
